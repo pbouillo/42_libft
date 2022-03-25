824ab01f4382e333eee514f60625454c0d199776
pbouillo <pbouillo@student.42.fr>
2022-03-25 13:03:52 +0100

    added atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,27 @@
 /*   By: pbouillo <pbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 13:13:48 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/03/23 13:13:49 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/03/25 12:47:01 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	sign;
+
+	res = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign *= -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + *str - '0';
+		str++;
+	}
+	return (res * sign);
+}
