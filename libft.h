a01129befc84df8c3d540654f4110003932b68c7
pbouillo <pbouillo@student.42.fr>
2022-04-04 17:27:19 +0200

    nc

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pbouillo <pbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 10:01:58 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/04/04 12:29:02 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/04/04 16:59:53 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,44 +22,68 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-int		ft_isprint(int c);
-int		ft_isalnum(int c);
+/* CTYPE | The ctype.h header file of the C Standard Library declares 
+several functions that are useful for testing and mapping characters. */
+
 int		ft_isalpha(int a);
+int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
 int		ft_isdigit(int a);
-int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_toupper(int c);
 
-int		ft_atoi(const char *str);
-char	*ft_itoa(int n);
+/* STRINGS (mem-functions) | The strings.h header file of the 
+C Standard Library declares several functions that are useful for manipulating 
+any type of arrays (but bytewise). */
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-int		ft_strlen(char *str);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-
-void	*ft_memchr(const void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
+/* PUTFUNCTIONS | */
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+/* STDLIB | The stdlib.h header defines four variable types, 
+several macros, and various functions for performing general functions. */
+
+int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_itoa(int n);
+
+/* STRING | The string.h header defines one variable type, one macro, 
+and various functions for manipulating arrays of characters. */
+
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strdup(char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strlen(char *str);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s1, char const *set);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	**ft_split(char const *s, char c);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
+/* LIST-FUNCTIONS | 
+The lst-funtcions allow you to manipulate and use lists. 
+Lists are quiet useful if you dont know the final length of an array 
+because you can just append as many nodes 
+as you until you run out of memory. */
+
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+t_list	*ft_lstnew(void *content);
 
 #endif
