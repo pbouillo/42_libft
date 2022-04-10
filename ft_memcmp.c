cb95b3e940f94a9d22658dff2703d8578c24d7ee
pbouillo <pbouillo@student.42.fr>
2022-04-10 17:34:57 +0200

    nc

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,11 +6,12 @@
 /*   By: pbouillo <pbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 10:55:30 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/04/04 17:32:29 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/04/10 17:13:00 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* returns the first difference between s1 and s2 */
+/* returns the first difference between s1 and s2 
+Both strings are assumed to be n size long */
 
 #include "libft.h"
 
