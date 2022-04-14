048caab91b5b84add81d20aa9232676674dd30a5
pbouillo <pbouillo@student.42.fr>
2022-04-14 15:40:18 +0200

    added mempy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,30 @@
 /*   By: pbouillo <pbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 16:20:24 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/04/04 17:32:31 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/04/14 15:39:08 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* copies n bytes from src to dst */
+/* copies n bytes from memory area src to memory area dst
+return the original value of dst */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned int	i;
+	unsigned char	*to;
+	unsigned char	*from;
+
+	to = (unsigned char *) dst;
+	from = (unsigned char *) src;
+	i = 0;
+	while (i < n)
+	{
+		if (to == NULL && from == NULL)
+			return (NULL);
+		to[i] = from[i];
+		i++;
+	}
+	return (to);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pbouillo <pbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 10:01:58 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/04/14 14:20:25 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/04/14 15:33:35 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ C Standard Library declares several functions that are useful for manipulating
 any type of arrays (but bytewise). */
 
 void	ft_bzero(void *s, size_t n);
-// void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 // void	*ft_memmove(void *dst, const void *src, size_t len);
