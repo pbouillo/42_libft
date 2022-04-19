691e56ea0277ff9da27533687a28f6d79aca09fd
pbouillo <pbouillo@student.42.fr>
2022-04-19 14:49:27 +0200

    added strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pbouillo <pbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 13:17:57 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/04/08 16:54:17 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/04/19 14:48:36 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,22 @@
 ’s1’ with the characters specified in ’set’ removed
 from the beginning and the end of the string.
 Returns the trimmed string; NULL if allocation fails 
-- creates a trimmed substring of *s without chars in set */
+- creates a trimmed substring of *s1 without chars in set */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	sz;
+	char	*retstr;
+
+	if (s1 == NULL && set == NULL)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	sz = ft_strlen(s1);
+	while (sz > 0 && ft_strchr(set, s1[sz]))
+		sz--;
+	retstr = ft_substr(s1, 0, sz + 1);
+	return (retstr);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pbouillo <pbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 10:01:58 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/04/19 11:43:01 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/04/19 14:30:20 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *str);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-// char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strtrim(char const *s1, char const *set);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 // char	**ft_split(char const *s, char c);
 // void	ft_striteri(char *s, void (*f)(unsigned int, char*)); 
