74208f1b08a624cc6e0014a24f333c85c12de6eb
pbouillo <pbouillo@student.42heilbronn.de>
2022-04-28 15:08:34 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -17,4 +17,23 @@ with a NULL pointer. */
 
 #include "libft.h"
 
+static int	word_counter(char const *s, char c)
+{
+	unsigned int	i;
+	int				cntr;
+
+	i = 0;
+	cntr = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			cntr++;
+		while (s[i] && (s[i] != c))
+			i++;
+	}
+	return (cntr);
+}
+
 char	**ft_split(char const *s, char c);
